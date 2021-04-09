61131549fc9b0954dca05f33558eea9f71a1078d
QSP <QSP@QSP>
2021-04-09 08:11:45 +0530

    Hi

---
+++ src/test/java/demojenkinsz/Test1.java
@@ -0,0 +1,12 @@
+package demojenkinsz;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void demo() {
+		Reporter.log("hello python",true);
+	}
+
+}
